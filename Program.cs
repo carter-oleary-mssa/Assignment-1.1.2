5220a73e6ffd3f1c8f04100157f102d72da47ed5
carter-oleary-mssa <carteroleary.mssa@gmail.com>
2025-04-07 17:12:28 -0400

    Assignment 1.1.2

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.Write("Enter two numbers: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The sum of {a} and {b} is {(a+b)}");
